e323c59588ca175a512f738a109f2338269b712d
daichi <user@daichi-mac.local>
2016-01-30 18:02:01 +0900

    commit

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class MyType
+{
+	public static float ObjectToFloat(object data)
+	{
+		float f = 0;
+		if (float.TryParse (data.ToString(), out f)) {
+			return f;
+		}
+
+		int i = 0;
+		if (int.TryParse (data.ToString(), out i)) {
+			return (float)i;
+		}
+		return 0;
+	}
+}
+
+public struct NoteData
+{
+	public float Radius;
+	public float Speed;
+	public string Message;
+	public NoteData(float Radius, float Speed, string Message)
+	{
+		this.Radius = Radius;
+		this.Speed = Speed;
+		this.Message = Message;
+	}
+	public NoteData(Dictionary<string, object> data)
+	{
+		Radius = MyType.ObjectToFloat (data ["Radius"]);
+		Speed = MyType.ObjectToFloat (data ["Speed"]);
+		Message = (string)data ["Message"];
+	}
+	public object Serialize()
+	{
+		var data = new Dictionary<string, object> ();
+		data ["Radius"] = Radius;
+		data ["Speed"] = Speed;
+		data ["Message"] = Message;
+		return data;
+	}
+};
+
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleController : MonoBehaviour
+{
+
+	public void GotoSokusin()
+	{
+		SceneManager.LoadScene ("main");
+	}
+
+}
