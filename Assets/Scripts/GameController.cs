6fa2533dcfd003127da5a6891637147b895f5b93
silver <cpisioep4@yahoo.co.jp>
2016-01-31 08:34:47 +0900

    Revert "StomacFactorを適用"
    This reverts commit 1350078b043844928d4a7100d961e42b414597cc.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -48,10 +48,10 @@ public class GameController : MonoBehaviour
 		}
 
 
-		Observable.Interval (TimeSpan.FromSeconds (5)).Subscribe (_ => {
+		Observable.Interval (TimeSpan.FromSeconds (8)).Subscribe (_ => {
 			SoundManager.Instance.PlaySE(e_SeSound.Goon);
 		}).AddTo(this.gameObject);
-
+		SoundManager.Instance.PlayBGM (e_BgmSound.Mokugyo);
 
 		timeCount = 0;
 		BuddhaWay = BuddhaState.human;
@@ -84,6 +84,11 @@ public class GameController : MonoBehaviour
 		GUIController.Instance.OnCoolTimeFinish = () => {
 			player.Hide(false);
 		};
+	}
+
+	void OnDestroy()
+	{
+		SoundManager.Instance.StopBGM ();
 	}
 
 	void Update()
@@ -134,8 +139,7 @@ public class GameController : MonoBehaviour
 
 	void DecreaseUpdate()
 	{
-        print(player.GetDecreaseStomacFactor());
-        player.Stomac -= (gameData.StomachMaxValue / gameData.StomachLostTime / 60f) * player.GetDecreaseStomacFactor();
+		player.Stomac -= gameData.StomachMaxValue / gameData.StomachLostTime / 60f;
 		SetStomac(player.Stomac / gameData.StomachMaxValue);
 	}
 
